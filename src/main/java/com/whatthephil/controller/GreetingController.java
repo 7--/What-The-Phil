b94fd22ec5c7ed3192ded23b5fc3342f2bf51617
7-- <regop412@gmail.com>
2020-02-06 22:31:17 -0500

    ok

--- src/main/java/com/whatthephil/controller/GreetingController.java
+++ src/main/java/com/whatthephil/controller/GreetingController.java
@@ -4,11 +4,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class GreetingController {
 
+	
+	@GetMapping("/.well-known/acme-challenge/92da9-VSaIB-FGZ8vXji0SNog0HOyP4581xR7xwhePM")
+	@ResponseBody
+	public String wellknown() {
+		return "92da9-VSaIB-FGZ8vXji0SNog0HOyP4581xR7xwhePM._C58mQk55lmZNmP0PSWBPQmTcPk4fBZWNuNgkpoKc3Q";
+	}
+	
+	
 	@GetMapping("/")
 	public String greeting() {
 		return "greeting";
