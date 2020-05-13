afe4170c5042ccf552ff35f08003445743663740
7-- <regop412@gmail.com>
2020-05-12 22:34:30 -0400

    finding rates POC

--- src/main/java/com/whatthephil/controller/GreetingController.java
+++ src/main/java/com/whatthephil/controller/GreetingController.java
@@ -2,11 +2,15 @@ package com.whatthephil.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
 import com.whatthephil.dto.BitmexFunding;
+import com.whatthephil.model.AvgFundingRates;
 
 @Controller
 public class GreetingController {
@@ -20,12 +24,27 @@ public class GreetingController {
 	
 	
 	@GetMapping("/")
-	public String greeting() {
-		
-		fundingCall();
-		
+	public String greeting(ModelMap model) {
+		double oneDayAvg = oneDayAvg(fundingCall());
+		model.addAttribute("oneDayAvg", oneDayAvg);
 		return "greeting";
 	}
+
+	private double oneDayAvg(BitmexFunding[] bitmexFundingAll) {
+		int dailyXrp = 3;
+		double dailyXrpAvg = 0;
+		for(BitmexFunding bitmexFunding: bitmexFundingAll) {
+			if(bitmexFunding.getSymbol().equals("XRPUSD")) {
+				if(dailyXrp>0) {
+					dailyXrpAvg += bitmexFunding.getFundingRate();
+					dailyXrp--;
+				}
+			}
+		}
+		dailyXrpAvg = (dailyXrpAvg * 100) / 3;
+		return dailyXrpAvg;
+	}
+
 
 	@GetMapping("/jstest")
 	public String jstest() {
@@ -38,10 +57,10 @@ public class GreetingController {
 		return "jstest";
 	}
 	
-	public void fundingCall() {
+	public BitmexFunding[] fundingCall() {
 		RestTemplate restTemplate = new RestTemplate();
 		String url = "https://www.bitmex.com/api/v1/funding?symbol=&count=500&reverse=true";
-		BitmexFunding[] bitmexFunding = restTemplate.getForObject(url, BitmexFunding[].class);
+		return restTemplate.getForObject(url, BitmexFunding[].class);
 	}
 
 }
---
+++ src/main/java/com/whatthephil/model/AvgFundingRates.java
@@ -0,0 +1,5 @@
+package com.whatthephil.model;
+
+public class AvgFundingRates {
+	
+}
