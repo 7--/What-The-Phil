b6f80c12aefb19e41cac1641dfaaec4360df1804
7-- <regop412@gmail.com>
2020-05-14 23:34:03 -0400

    more avg rates

--- src/main/java/com/whatthephil/controller/GreetingController.java
+++ src/main/java/com/whatthephil/controller/GreetingController.java
@@ -15,48 +15,49 @@ import com.whatthephil.model.AvgFundingRates;
 @Controller
 public class GreetingController {
 
-	
 	@GetMapping("/.well-known/acme-challenge/92da9-VSaIB-FGZ8vXji0SNog0HOyP4581xR7xwhePM")
 	@ResponseBody
 	public String wellknown() {
 		return "92da9-VSaIB-FGZ8vXji0SNog0HOyP4581xR7xwhePM._C58mQk55lmZNmP0PSWBPQmTcPk4fBZWNuNgkpoKc3Q";
 	}
-	
-	
+
 	@GetMapping("/")
 	public String greeting(ModelMap model) {
-		double oneDayAvg = oneDayAvg(fundingCall());
-		model.addAttribute("oneDayAvg", oneDayAvg);
+		AvgFundingRates avgFundingRates = oneDayAvg(fundingCall());
+		model.addAttribute("avgFundingRates", avgFundingRates);
 		return "greeting";
 	}
 
-	private double oneDayAvg(BitmexFunding[] bitmexFundingAll) {
-		int dailyXrp = 3;
-		double dailyXrpAvg = 0;
-		for(BitmexFunding bitmexFunding: bitmexFundingAll) {
-			if(bitmexFunding.getSymbol().equals("XRPUSD")) {
-				if(dailyXrp>0) {
-					dailyXrpAvg += bitmexFunding.getFundingRate();
-					dailyXrp--;
-				}
+	private AvgFundingRates oneDayAvg(BitmexFunding[] bitmexFundingAll) {
+		AvgFundingRates avgFundingRates = new AvgFundingRates();
+		int dailyXrpCount = 3;
+		double dailyXrp = 0;
+		int threeDayXrpCount = 9;
+		double threeDayXrp = 0;
+		int weeklyXrpCount = 21;
+		double weeklyXrp = 0;
+		for (BitmexFunding bitmexFunding : bitmexFundingAll) {
+			if (bitmexFunding.getSymbol().equals("XRPUSD")) {
+				if (weeklyXrpCount > 0) {
+					if (threeDayXrpCount > 0) {
+						if (dailyXrpCount > 0) {
+							dailyXrp += bitmexFunding.getFundingRate();
+							dailyXrpCount--;
+						}
+						threeDayXrp += bitmexFunding.getFundingRate();
+						threeDayXrpCount--;
+					}
+					weeklyXrp += bitmexFunding.getFundingRate();
+					weeklyXrpCount--;
+				}		
 			}
 		}
-		dailyXrpAvg = (dailyXrpAvg * 100) / 3;
-		return dailyXrpAvg;
+		avgFundingRates.setXrpOneDayAvg((dailyXrp * 100) / 3);
+		avgFundingRates.setXrpThreeDayAvg((threeDayXrp * 100) / 9);
+		avgFundingRates.setXrpWeeklyAvg((weeklyXrp * 100) / 21);
+		return avgFundingRates;
 	}
 
-
-	@GetMapping("/jstest")
-	public String jstest() {
-		RestTemplate restTemplate = new RestTemplate();
-		String fooResourceUrl = "https://api.binance.com/api/v3/depth?symbol=XTZUSDT&limit=5000";
-		ResponseEntity<String> response = restTemplate.getForEntity(fooResourceUrl, String.class);
-		String respsone = response.getBody();
-		System.out.println(response);
-		
-		return "jstest";
-	}
-	
 	public BitmexFunding[] fundingCall() {
 		RestTemplate restTemplate = new RestTemplate();
 		String url = "https://www.bitmex.com/api/v1/funding?symbol=&count=500&reverse=true";
--- src/main/java/com/whatthephil/model/AvgFundingRates.java
+++ src/main/java/com/whatthephil/model/AvgFundingRates.java
@@ -1,5 +1,32 @@
 package com.whatthephil.model;
 
 public class AvgFundingRates {
+	private double xrpOneDayAvg;
+	private double xrpThreeDayAvg;
+	private double xrpWeeklyAvg;
+
+	public double getXrpOneDayAvg() {
+		return xrpOneDayAvg;
+	}
+
+	public void setXrpOneDayAvg(double xrpOneDayAvg) {
+		this.xrpOneDayAvg = xrpOneDayAvg;
+	}
+
+	public double getXrpThreeDayAvg() {
+		return xrpThreeDayAvg;
+	}
+
+	public void setXrpThreeDayAvg(double xrpThreeDayAvg) {
+		this.xrpThreeDayAvg = xrpThreeDayAvg;
+	}
+
+	public double getXrpWeeklyAvg() {
+		return xrpWeeklyAvg;
+	}
+
+	public void setXrpWeeklyAvg(double xrpWeeklyAvg) {
+		this.xrpWeeklyAvg = xrpWeeklyAvg;
+	}
 	
 }
